b5e0a37771fdd989513b496f9b0384600e5411b8
Damian Modernell <dmodernell@cohesionib.com>
2024-11-12 17:44:44 -0300

    Implementing ConsoleLogger

---
+++ ISSTracker/Logging/ConsoleLogger.swift
@@ -0,0 +1,19 @@
+//
+//  ConsoleLogger.swift
+//  ISSTracker
+//
+//  Created by Damian Modernell on 12/11/24.
+//  Copyright © 2024 Damian Modernell. All rights reserved.
+//
+
+import Foundation
+
+class ConsoleLogger {
+    static let sharedInstance = ConsoleLogger()
+    
+    private init() {}
+    
+    func logEvent(_ message: String) {
+        print(message)
+    }
+}
